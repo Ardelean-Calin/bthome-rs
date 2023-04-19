5403100c2a005d10a71d757166829c9b70a6d1b6
Ardelean Calin <artech@duck.com>
2023-04-19 12:50:38 +0300

    Re-implemented everything to be more easy to use.

--- src/lib.rs
+++ src/lib.rs
@@ -1,308 +1,181 @@
 #![no_std]
 
-use defmt::Format;
-use heapless::Vec;
+#[macro_use]
+pub mod macros;
 
-#[derive(Format, Debug)]
-pub enum BTHomeError {
-    UnknownObjectID,
-    BufferFull,
-}
-
-pub trait BTHome<const N: usize> {
-    const OBJECT_ID: u8;
-    fn as_vec(&mut self) -> Result<Vec<u8, N>, BTHomeError>;
-    fn length(&self) -> usize;
-}
-
+#[rustfmt::skip]
+#[allow(non_camel_case_types)]
 pub mod fields {
-    use crate::{BTHome, BTHomeError};
-    use defmt::Format;
-    use heapless::Vec;
+    //          Field Name              ID    Internal Representation   External Representation
+    impl_field!(PacketID,               0x00, [u8; 2],                  u8);
+    impl_field!(Battery_1Per,           0x01, [u8; 2],                  u8);
+    impl_field!(CO2_ppm,                0x12, [u8; 3],                  u16);
+    impl_field!(Count_1byte,            0x09, [u8; 2],                  u8);
+    impl_field!(Count_2bytes,           0x3D, [u8; 3],                  u16);
+    impl_field!(Count_4bytes,           0x3E, [u8; 5],                  u32);
+    impl_field!(Current_1mA,            0x43, [u8; 3],                  u16);
+    impl_field!(Dewpoint_10mK,          0x08, [u8; 3],                  i16);
+    impl_field!(Distance_100mm,         0x41, [u8; 3],                  u16);
+    impl_field!(Distance_1mm,           0x40, [u8; 3],                  u16);
+    impl_field!(Duration_1ms,           0x42, [u8; 4],                  u32);
+    impl_field!(Energy_1Wh,             0x0A, [u8; 4],                  u32);
+    impl_field!(Humidity_10mPer,        0x03, [u8; 3],                  u16);
+    impl_field!(Humidity_1Per,          0x2E, [u8; 2],                  u8);
+    impl_field!(Illuminance_10mLux,     0x05, [u8; 4],                  u32);
+    impl_field!(Mass_10g,               0x06, [u8; 3],                  u16);
+    impl_field!(Mass_10mLb,             0x07, [u8; 3],                  u16);
+    impl_field!(Moisture_10mPer,        0x14, [u8; 3],                  u16);
+    impl_field!(Moisture_1Per,          0x2F, [u8; 2],                  u8);
+    impl_field!(PM10_1ugmc,             0x0E, [u8; 3],                  u16);
+    impl_field!(PM25_1ugmc,             0x0D, [u8; 3],                  u16);
+    impl_field!(Power_10mW,             0x0B, [u8; 4],                  u32);
+    impl_field!(Pressure_1Pa,           0x04, [u8; 4],                  u32);
+    impl_field!(Rotation_100mDeg,       0x3F, [u8; 3],                  i16);
+    impl_field!(Speed_10mms,            0x44, [u8; 3],                  u16);
+    impl_field!(Temperature_100mK,      0x45, [u8; 3],                  i16);
+    impl_field!(Temperature_10mK,       0x02, [u8; 3],                  i16);
+    impl_field!(TVOC_1ugmc,             0x13, [u8; 3],                  u16);
+    impl_field!(UVIndex_100milli,       0x46, [u8; 2],                  u8);
+    impl_field!(Voltage_100mV,          0x4A, [u8; 3],                  u16);
+    impl_field!(Voltage_1mV,            0x0C, [u8; 3],                  u16);
+    impl_field!(Volume_100mL,           0x47, [u8; 3],                  u16);
+    impl_field!(Volume_1mL,             0x48, [u8; 3],                  u16);
+    impl_field!(VolumeFlowRate_1LpH,    0x49, [u8; 3],                  u16);
 
-    const FIELD_SIZE: usize = 5;
-
-    macro_rules! impl_bthome_field {
-        ($name:ident, $obj_id:literal, $factor:literal, $type:ty, $opt_actual_size:expr) => {
-            #[allow(non_camel_case_types)]
-            #[derive(Format, Copy, Clone, Debug, PartialEq)]
-            pub struct $name {
-                value: f32,
-            }
-
-            impl From<f32> for $name {
-                fn from(value: f32) -> Self {
-                    $name { value }
-                }
-            }
-
-            impl From<$name> for f32 {
-                fn from(field: $name) -> f32 {
-                    field.value
-                }
-            }
-
-            impl BTHome<FIELD_SIZE> for $name {
-                const OBJECT_ID: u8 = $obj_id;
-
-                fn as_vec(&mut self) -> Result<Vec<u8, FIELD_SIZE>, BTHomeError> {
-                    let mut byte_array = Vec::<u8, FIELD_SIZE>::new();
-                    byte_array
-                        .push(Self::OBJECT_ID)
-                        .map_err(|_| BTHomeError::BufferFull)?;
-
-                    let bytes = ((self.value * $factor) as $type).to_le_bytes();
-                    byte_array
-                        .extend_from_slice(&bytes)
-                        .map_err(|_| BTHomeError::BufferFull)?;
-
-                    if let Some(i) = $opt_actual_size {
-                        Ok(
-                            Vec::<u8, FIELD_SIZE>::from_slice(byte_array.split_at(i + 1).0)
-                                .unwrap(),
-                        )
-                    } else {
-                        Ok(byte_array)
-                    }
-                }
-
-                #[inline]
-                fn length(&self) -> usize {
-                    if let Some(i) = $opt_actual_size {
-                        (i as usize) + 1
-                    } else {
-                        core::mem::size_of::<$type>() + 1
-                    }
-                }
-            }
-        };
-        ($name:ident, $obj_id:literal, $factor:literal, $type:ty) => {
-            impl_bthome_field!($name, $obj_id, $factor, $type, None::<usize>);
-        };
-    }
-
-    impl_bthome_field!(PacketID, 0x00, 1.0, u8);
-    impl_bthome_field!(Battery_1Per, 0x01, 1.0, u8);
-    impl_bthome_field!(CO2_ppm, 0x12, 1.0, u16);
-    impl_bthome_field!(Count_1byte, 0x09, 1.0, u8);
-    impl_bthome_field!(Count_2bytes, 0x3D, 1.0, u16);
-    impl_bthome_field!(Count_4bytes, 0x3E, 1.0, u32);
-    impl_bthome_field!(Current_1mA, 0x43, 1000.0, u16);
-    impl_bthome_field!(Dewpoint_10mK, 0x08, 100.0, i16);
-    impl_bthome_field!(Distance_100mm, 0x41, 10.0, u16);
-    impl_bthome_field!(Distance_1mm, 0x40, 1.0, u16);
-    impl_bthome_field!(Duration_1ms, 0x42, 1000.0, u32, Some(3));
-    impl_bthome_field!(Energy_1Wh, 0x0A, 1000.0, u32, Some(3));
-    impl_bthome_field!(Humidity_10mPer, 0x03, 100.0, u16);
-    impl_bthome_field!(Humidity_1Per, 0x2E, 1.0, u8);
-    impl_bthome_field!(Illuminance_10mLux, 0x05, 100.0, u32, Some(3));
-    impl_bthome_field!(Mass_10g, 0x06, 100.0, u16);
-    impl_bthome_field!(Mass_10mLb, 0x07, 100.0, u16);
-    impl_bthome_field!(Moisture_10mPer, 0x14, 100.0, u16);
-    impl_bthome_field!(Moisture_1Per, 0x2F, 1.0, u8);
-    impl_bthome_field!(PM10_1ugmc, 0x0E, 1.0, u16);
-    impl_bthome_field!(PM25_1ugmc, 0x0D, 1.0, u16);
-    impl_bthome_field!(Power_10mW, 0x0B, 100.0, u32, Some(3));
-    impl_bthome_field!(Pressure_1Pa, 0x04, 100.0, u32, Some(3));
-    impl_bthome_field!(Rotation_100mDeg, 0x3F, 10.0, i16);
-    impl_bthome_field!(Speed_10mms, 0x44, 100.0, u16);
-    impl_bthome_field!(Temperature_100mK, 0x45, 10.0, i16);
-    impl_bthome_field!(Temperature_10mK, 0x02, 100.0, i16);
-    impl_bthome_field!(TVOC_1ugmc, 0x13, 1.0, u16);
-    impl_bthome_field!(UVIndex_100milli, 0x46, 10.0, u8);
-    impl_bthome_field!(Voltage_100mV, 0x4A, 10.0, u16);
-    impl_bthome_field!(Voltage_1mV, 0x0C, 1000.0, u16);
-    impl_bthome_field!(Volume_100mL, 0x47, 10.0, u16);
-    impl_bthome_field!(Volume_1mL, 0x48, 1.0, u16);
-    impl_bthome_field!(VolumeFlowRate_1LpH, 0x49, 1000.0, u16);
 
     #[cfg(test)]
     mod tests {
-        extern crate std;
         use super::*;
 
         #[test]
-        fn test_cast_from_f32() {
-            let expected = Battery_1Per { value: 10.2 };
-            let field: Battery_1Per = 10.2.into();
-            assert_eq!(field, expected);
+        fn test_full() {
+            // A simple 1-byte field
+            let field = PacketID::from(0x23);
+            assert_eq!(field.0, [0x00u8, 0x23u8]);
+            assert_eq!(field.get(), 0x23u8);
 
-            let expected: f32 = 10.2;
-            let casted: f32 = field.into();
-            assert_eq!(casted, expected);
-        }
+            let field =  Battery_1Per::from(55);
+            assert_eq!(field.0, [0x01, 55u8]);
+            assert_eq!(field.get(), 55u8);
 
-        #[test]
-        fn test_as_vec() {
-            let mut field: Moisture_10mPer = 33.5f32.into();
-            let expected = std::vec![0x14u8, 0x16u8, 0x0Du8];
-            let arr = field.as_vec().unwrap();
-            assert_eq!(expected, arr.as_slice());
+            // More complex, 2-byte signed int field
+            // Convert positive temperature to bytes
+            let temperature = 27.3f32;
+            let temp_100mk = temperature * 10f32;
+            let temp_field = Temperature_100mK::from(temp_100mk as i16);
+            assert_eq!(temp_field.0, [0x45u8, 0x11u8, 0x01u8]);
+            // And back
+            let temp_100mk: f32 = temp_field.get() as f32;
+            assert_eq!(temp_100mk, 273.0f32);
 
-            // Positive temperature
-            let mut field: Temperature_100mK = 27.3f32.into();
-            let expected = std::vec![0x45u8, 0x11u8, 0x01u8];
-            let arr = field.as_vec().unwrap();
-            assert_eq!(expected, arr.as_slice());
+            // Convert negative temperature to bytes
+            let temperature = -25.2f32;
+            let temp_100mk = temperature * 10f32;
+            let temp_field = Temperature_100mK::from(temp_100mk as i16);
+            assert_eq!(temp_field.0, [0x45u8, 0x04u8, 0xFFu8]);
+            // And back
+            let temp_100mk: f32 = temp_field.get() as f32;
+            assert_eq!(temp_100mk, -252.0f32);
 
-            // Negative temperature [0.1°C]
-            let mut field: Temperature_100mK = (-25.2f32).into();
-            let expected = std::vec![0x45u8, 0x04u8, 0xFFu8];
-            let arr = field.as_vec().unwrap();
-            assert_eq!(expected, arr.as_slice());
+            // A 3-byte field
+            let lux = 13460.67f32;
+            let lux_10mlx = (lux * 100f32) as u32;
+            let field = Illuminance_10mLux::from(lux_10mlx);
+            assert_eq!(field.0, [0x05, 0x13, 0x8A, 0x14]);
+            let lux_10mlx = field.get() as f32;
+            assert_eq!(lux_10mlx, 1346067f32);
 
-            // Negative temperature [0.01°C]
-            let mut field: Temperature_10mK = (-25.06f32).into();
-            let expected = std::vec![0x02u8, 0x36u8, 0xF6u8];
-            let arr = field.as_vec().unwrap();
-            assert_eq!(expected, arr.as_slice());
-
-            // 3-byte long fields
-            let mut field: Illuminance_10mLux = 13460.67.into();
-            let expected: std::vec::Vec<u8> = std::vec![0x05, 0x13, 0x8A, 0x14];
-            let arr = field.as_vec().unwrap();
-            assert_eq!(expected, arr.as_slice());
         }
 
         #[test]
         fn test_length() {
-            // 1 byte long + 1 byte ID
-            let field: Battery_1Per = 69.0.into();
-            let expected: usize = 2;
-            assert_eq!(field.length(), expected);
-
-            // 2 bytes long + 1 byte ID
-            let field: Distance_100mm = 10.0.into();
-            let expected: usize = 3;
-            assert_eq!(field.length(), expected);
-
-            // 3 bytes long + 1 byte ID
-            let field: Illuminance_10mLux = 1234.0.into();
-            let expected: usize = 4;
-            assert_eq!(field.length(), expected);
-
-            // 4 bytes long + 1 byte ID
-            let field: Count_4bytes = 12345678.0.into();
-            let expected: usize = 5;
-            assert_eq!(field.length(), expected);
+            assert_eq!(core::mem::size_of::<Battery_1Per>(), 2);
+            assert_eq!(core::mem::size_of::<Distance_100mm>(), 3);
+            assert_eq!(core::mem::size_of::<Illuminance_10mLux>(), 4);
+            assert_eq!(core::mem::size_of::<Count_4bytes>(), 5);
         }
     }
 }
+
+#[rustfmt::skip]
+#[allow(non_camel_case_types)]
 pub mod flags {
-    use crate::{BTHome, BTHomeError};
-    use defmt::Format;
-    use heapless::Vec;
-
-    const FLAG_SIZE: usize = 2;
-
-    macro_rules! impl_bthome_flag {
-        ($name:ident, $obj_id:literal) => {
-            #[allow(non_camel_case_types)]
-            #[derive(Format, Debug, Copy, Clone, PartialEq)]
-            pub struct $name {
-                value: bool,
-            }
-
-            impl BTHome<FLAG_SIZE> for $name {
-                const OBJECT_ID: u8 = $obj_id;
-
-                fn as_vec(&mut self) -> Result<heapless::Vec<u8, FLAG_SIZE>, crate::BTHomeError> {
-                    let mut byte_array = Vec::<u8, FLAG_SIZE>::new();
-                    byte_array
-                        .push(Self::OBJECT_ID)
-                        .map_err(|_| BTHomeError::BufferFull)?;
-                    byte_array
-                        .push(self.value.into())
-                        .map_err(|_| BTHomeError::BufferFull)?;
-
-                    Ok(byte_array)
-                }
-
-                fn length(&self) -> usize {
-                    2
-                }
-            }
-
-            impl From<bool> for $name {
-                fn from(value: bool) -> Self {
-                    $name { value }
-                }
-            }
-
-            impl From<$name> for bool {
-                fn from(field: $name) -> bool {
-                    field.value
-                }
-            }
-        };
-    }
-
-    impl_bthome_flag!(Battery, 0x15);
-    impl_bthome_flag!(Battery_Charging, 0x16);
-    impl_bthome_flag!(Carbon_Monoxide, 0x17);
-    impl_bthome_flag!(Cold, 0x18);
-    impl_bthome_flag!(Connectivity, 0x19);
-    impl_bthome_flag!(Door, 0x1A);
-    impl_bthome_flag!(Garage_Door, 0x1B);
-    impl_bthome_flag!(Gas, 0x1C);
-    impl_bthome_flag!(Generic_Boolean, 0x0F);
-    impl_bthome_flag!(Heat, 0x1D);
-    impl_bthome_flag!(Light, 0x1E);
-    impl_bthome_flag!(Lock, 0x1F);
-    impl_bthome_flag!(Moisture, 0x20);
-    impl_bthome_flag!(Motion, 0x21);
-    impl_bthome_flag!(Moving, 0x22);
-    impl_bthome_flag!(Occupancy, 0x23);
-    impl_bthome_flag!(Opening, 0x11);
-    impl_bthome_flag!(Plugged_In, 0x24);
-    impl_bthome_flag!(Power, 0x10);
-    impl_bthome_flag!(Presence, 0x25);
-    impl_bthome_flag!(Problem, 0x26);
-    impl_bthome_flag!(Running, 0x27);
-    impl_bthome_flag!(Safety, 0x28);
-    impl_bthome_flag!(Smoke, 0x29);
-    impl_bthome_flag!(Sound, 0x2A);
-    impl_bthome_flag!(Tamper, 0x2B);
-    impl_bthome_flag!(Vibration, 0x2C);
-    impl_bthome_flag!(Window, 0x2D);
+    //         Flag Name            ID 
+    impl_flag!(Battery,             0x15);
+    impl_flag!(Battery_Charging,    0x16);
+    impl_flag!(Carbon_Monoxide,     0x17);
+    impl_flag!(Cold,                0x18);
+    impl_flag!(Connectivity,        0x19);
+    impl_flag!(Door,                0x1A);
+    impl_flag!(Garage_Door,         0x1B);
+    impl_flag!(Gas,                 0x1C);
+    impl_flag!(Generic_Boolean,     0x0F);
+    impl_flag!(Heat,                0x1D);
+    impl_flag!(Light,               0x1E);
+    impl_flag!(Lock,                0x1F);
+    impl_flag!(Moisture,            0x20);
+    impl_flag!(Motion,              0x21);
+    impl_flag!(Moving,              0x22);
+    impl_flag!(Occupancy,           0x23);
+    impl_flag!(Opening,             0x11);
+    impl_flag!(Plugged_In,          0x24);
+    impl_flag!(Power,               0x10);
+    impl_flag!(Presence,            0x25);
+    impl_flag!(Problem,             0x26);
+    impl_flag!(Running,             0x27);
+    impl_flag!(Safety,              0x28);
+    impl_flag!(Smoke,               0x29);
+    impl_flag!(Sound,               0x2A);
+    impl_flag!(Tamper,              0x2B);
+    impl_flag!(Vibration,           0x2C);
+    impl_flag!(Window,              0x2D);
 
     #[cfg(test)]
     mod tests {
-        extern crate std;
         use super::*;
 
         #[test]
-        fn test_cast_from_bool() {
-            let expected = Battery_Charging { value: true };
-            let flag: Battery_Charging = true.into();
-            assert_eq!(flag, expected);
+        fn test_full() {
+            let flag = Battery_Charging::from(true);
+            assert_eq!(flag.0, [0x16u8, 0x01]);
+            assert_eq!(flag.get(), true);
 
-            let expected: bool = true;
-            let casted: bool = flag.into();
-            assert_eq!(casted, expected);
-        }
-
-        #[test]
-        fn test_as_vec() {
-            // Battery charging flag.
-            let mut flag: Battery_Charging = true.into();
-            let expected = std::vec![0x16u8, 0x01];
-            let arr = flag.as_vec().unwrap();
-            assert_eq!(expected, arr.as_slice());
-
-            // Plugged in flag.
-            let mut flag: Plugged_In = false.into();
-            let expected = std::vec![0x24u8, 0x00u8];
-            let arr = flag.as_vec().unwrap();
-            assert_eq!(expected, arr.as_slice());
+            let flag = Plugged_In::from(false);
+            assert_eq!(flag.0, [0x24u8, 0x00u8]);
+            assert_eq!(flag.get(), false);
         }
 
         #[test]
         fn test_length() {
-            // 1 byte long + 1 byte ID
-            let flag: Generic_Boolean = false.into();
-            let expected: usize = 2;
-            assert_eq!(flag.length(), expected);
+            assert_eq!(core::mem::size_of::<Battery_Charging>(), 2);
+            assert_eq!(core::mem::size_of::<Plugged_In>(), 2);
+            assert_eq!(core::mem::size_of::<Problem>(), 2);
+            assert_eq!(core::mem::size_of::<Presence>(), 2);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate std;
+
+    #[test]
+    fn test_bthome_ad() {
+        build_bthome_ad!(
+            struct MyAD {
+                air_temperature: fields::Temperature_100mK,
+                air_humidity: fields::Humidity_1Per,
+                battery_charging: flags::Battery_Charging,
+            }
+        );
+
+        let my_ad = MyAD::default()
+            .air_humidity(50u8.into()) // 50%
+            .air_temperature(255i16.into()) // 25.5 degrees C
+            .battery_charging(true.into()); // A flag indicating that the battery is charging
+
+        let expected = std::vec![2, 1, 6, 11, 22, 210, 252, 64, 69, 255, 0, 46, 50, 22, 1];
+        assert_eq!(expected.as_slice(), my_ad.as_vec().as_slice());
     }
 }
---
+++ src/macros.rs
@@ -0,0 +1,158 @@
+/// Asserts that constant expressions evaluate to `true`.
+///
+/// Constant expressions can be ensured to have certain properties via this
+/// macro If the expression evaluates to `false`, the file will fail to compile.
+/// This is synonymous to [`static_assert` in C++][static_assert].
+#[macro_export]
+macro_rules! const_assert {
+    ($x:expr $(,)?) => {
+        #[allow(unknown_lints, eq_op)]
+        const _: [(); 0 - !{
+            const ASSERT: bool = $x;
+            ASSERT
+        } as usize] = [];
+    };
+}
+
+/// # Normal usecase:
+///
+/// ```rust
+/// use bthome;
+/// use bthome::build_bthome_ad;
+/// use bthome::const_assert;
+/// build_bthome_ad!(
+///     struct CompileFail {
+///         field1: bthome::fields::Count_4bytes,
+///         field2: bthome::fields::Count_4bytes,
+///         field3: bthome::fields::Count_4bytes,
+///     }
+/// );
+/// ```
+/// # Automatic overflow detection if payload goes over 23 bytes (TODO BLE5 with more bytes):
+///
+/// ```compile_fail
+/// use bthome;
+/// use bthome::build_bthome_ad;
+/// use bthome::const_assert;
+/// build_bthome_ad!(
+///     struct CompileFail {
+///         field1: bthome::fields::Count_4bytes,
+///         field2: bthome::fields::Count_4bytes,
+///         field3: bthome::fields::Count_4bytes,
+///         field4: bthome::fields::Count_4bytes,
+///         field5: bthome::fields::Count_4bytes,
+///         field6: bthome::fields::Count_4bytes,
+///         field7: bthome::fields::Count_4bytes,
+///     }
+/// );
+/// ```
+#[macro_export]
+macro_rules! build_bthome_ad {
+    (struct $name:ident {
+        $($field:ident: $ty:path,)*
+    }) => {
+        #[derive(Default, Clone, defmt::Format)]
+        pub struct $name {
+            $(
+                $field: Option<$ty>,
+            )*
+        }
+
+        impl $name {
+            const _TOTAL_SIZE: usize = 0 $(+ core::mem::size_of::<$ty>())*;
+
+            $(
+                pub fn $field(self, $field: $ty) -> Self {
+                    Self {
+                        $field: Some($field),
+                        ..self
+                    }
+                }
+            )*
+
+            pub fn as_vec(&self) -> heapless::Vec<u8, 31>{
+                let mut buf: heapless::Vec<u8, 31> = heapless::Vec::new();
+                buf.extend_from_slice(&[
+                    0x02,
+                    0x01,
+                    0x06, // BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE
+                ]).unwrap();
+
+                // The BTHome Header
+                let header = [
+                    0x04, // Size of the BTHome AD. Minimum 4 for the header.
+                    0x16,
+                    0xD2,
+                    0xFC,
+                    0x40,
+                ];
+                buf.extend_from_slice(&header).unwrap();
+
+                $(
+                    if let Some($field) = self.$field.clone() {
+                        buf.extend_from_slice($field.as_slice()).unwrap();
+                        buf[3] += core::mem::size_of::<$ty>() as u8;
+                    }
+                )*
+
+                buf
+            }
+        }
+
+        const_assert!($name::_TOTAL_SIZE <= 23);
+    };
+}
+
+macro_rules! impl_field {
+    ($name:ident, $id:literal, $internal_repr:ty, $external_repr:ty) => {
+        #[derive(defmt::Format, Clone)]
+        pub struct $name($internal_repr);
+
+        impl $name {
+            const ID: u8 = $id;
+            const SIZE: usize = core::mem::size_of::<$internal_repr>() - 1;
+
+            pub fn get(&self) -> $external_repr {
+                let mut bytes = [0u8; core::mem::size_of::<$external_repr>()];
+                bytes[0..Self::SIZE].clone_from_slice(&self.0[1..]);
+                <$external_repr>::from_le_bytes(bytes)
+            }
+
+            pub fn as_slice(&self) -> &[u8] {
+                &self.0
+            }
+        }
+
+        impl From<$external_repr> for $name {
+            fn from(value: $external_repr) -> Self {
+                let mut bytes = [0u8; core::mem::size_of::<$internal_repr>()];
+                bytes[0] = Self::ID;
+                bytes[1..].clone_from_slice(&value.to_le_bytes()[0..Self::SIZE]);
+                $name(bytes)
+            }
+        }
+    };
+}
+
+macro_rules! impl_flag {
+    ($name:ident, $id:literal) => {
+        #[derive(defmt::Format, Clone)]
+        pub struct $name([u8; 2]);
+        impl $name {
+            pub fn get(&self) -> bool {
+                self.0[1] != 0
+            }
+
+            pub fn as_slice(&self) -> &[u8] {
+                &self.0
+            }
+        }
+
+        impl From<bool> for $name {
+            fn from(value: bool) -> Self {
+                let bytes: [u8; 2] = [$id, value as u8];
+                $name(bytes)
+            }
+        }
+    };
+}
